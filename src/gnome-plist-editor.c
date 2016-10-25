d70321d613c448abc175e39a28789134d493e430
Timothy Ward <gtwa001@gmail.com>
2016-10-25 19:24:47 +1100

    Source file clean-up
    Signed-off-by: Timothy Ward <gtwa001@gmail.com>

--- src/gnome-plist-editor.c
+++ src/gnome-plist-editor.c
@@ -409,16 +409,16 @@ void about_menu_item_activate_cb(GtkMenuItem *item, gpointer user_data) {
         };
         
         gtk_show_about_dialog(app.main_window,
-		"name", "Gnome Property List Editor",
-        "version", PACKAGE_VERSION, 
+        "name", "Gnome Property List Editor",
+        "version", PACKAGE_VERSION,
         "comments", "A developer's Property List Editor",
         "authors", authors,
-		"logo-icon-name", "text-editor",
-		"website", "http://cgit.sukimashita.com/gnome-plist-editor.git/",
-		"website-label", "GIT Source Code Repository",
-		"license-type", GTK_LICENSE_LGPL_2_1,
+        "logo-icon-name", "text-editor",
+        "website", "http://cgit.sukimashita.com/gnome-plist-editor.git/",
+        "website-label", "GIT Source Code Repository",
+        "license-type", GTK_LICENSE_LGPL_2_1,
         "wrap-license", "TRUE",
-		NULL);
+        NULL);
 }
 
 void plist_cell_data_function (GtkTreeViewColumn *col,
